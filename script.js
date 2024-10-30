b2240218c2ba2377546a8b85b6054097e6c14e65
JDoyle23 <joshdoyle2013@outlook.com>
2024-10-30 13:17:32 +0000

    feat: update to play the game using buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//gets Computer choice
 function getComputerChoice() {
     let computerOptions = ["rock", "paper", "scissors"];
     let computerOptionsLength = computerOptions.length;
@@ -11,39 +12,117 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Please make your choice: rock, paper or scissors?");
-    return humanChoice.toLowerCase();
-}
-
+//initializes score variables
 let humanScore = 0;
 let computerScore = 0;
 let gameNumber = 0;
 
-//create function playRound to compare the two choices and increment scores accordingly
-function playRound(human, computer) {
-    if (human === "rock" && computer === "rock" || human === "scissors" && computer === "scissors" || human === "paper" && computer === "paper") {
-        alert(`You draw! Your choice: ${human} Computer choice: ${computer}`);
+
+//declares querySelectors
+const resultsDiv = document.querySelector("#results");
+const scoreDiv = document.querySelector("#score");
+const div = document.createElement("div");
+const span = document.createElement("span");
+const resultSpan = document.createElement("span");
+const errorSpan = document.createElement("span");
+let btns = document.querySelectorAll('button');
+
+//increments the game number
+btns.forEach(function (i) {
+    i.addEventListener('click', () => {
         gameNumber++;
-    } else if (human === "rock" && computer === "scissors" || human === "paper" && computer === "rock" || human === "scissors" && computer === "paper") {
-        alert(`You win! Your choice: ${human} Computer choice: ${computer}`);
-        humanScore++;
-        gameNumber++;        
-    } else {
-        alert(`You lose! Your choice: ${human} Computer choice: ${computer}`);
-       computerScore++;
-       gameNumber++;
-    } 
-}
+    })
+    
+})
 
-function game () {
-    for (let i = 0; i < 5; i++) {
-        playRound(getHumanChoice(), getComputerChoice());
+//plays Round using buttons
+function playRound(human, computer) {
+        if (human === "rock" && computer === "rock" || human === "scissors" && computer === "scissors" || human === "paper" && computer === "paper") {
+            resultsDiv.appendChild(div);
+            div.textContent = `You draw! Your choice: ${human} Computer choice: ${computer}` 
+        } else if (human === "rock" && computer === "scissors" || human === "paper" && computer === "rock" || human === "scissors" && computer === "paper") {
+            resultsDiv.appendChild(div);
+            div.textContent = `You win! Your choice: ${human} Computer choice: ${computer}`
+            humanScore++;
+        } else {
+            resultsDiv.appendChild(div);
+            div.textContent = `You lose! Your choice: ${human} Computer choice: ${computer}`
+            computerScore++;  
+        }
     }
-}
 
-game();
 
-console.log("Human score: " + humanScore);
-console.log("Computer score: " + computerScore);
-console.log("Game number: " + gameNumber);
+//initialises query selectors
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+//plays game using buttons
+rock.addEventListener("click", () => {
+    if (gameNumber < 6) {
+        playRound("rock", getComputerChoice());
+        scoreDiv.appendChild(span);
+        span.textContent = `Game number: ${gameNumber} Human score: ${humanScore} Computer score: ${computerScore}`
+    } else if (gameNumber = 5) {
+        playRound("paper", getComputerChoice());
+        scoreDiv.appendChild(span);
+        span.textContent = `Game number: ${gameNumber} Human score: ${humanScore} Computer score: ${computerScore}`
+        if (humanScore > computerScore) {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You win!"
+        } else if (computerScore > humanScore) {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You lose!"
+        } else {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You draw!"
+        }
+    } 
+    
+})
+
+paper.addEventListener("click", () => {
+    if (gameNumber < 5) {
+        playRound("paper", getComputerChoice());
+        scoreDiv.appendChild(span);
+        span.textContent = `Game number: ${gameNumber} Human score: ${humanScore} Computer score: ${computerScore}`
+    } else if (gameNumber = 5) {
+    playRound("paper", getComputerChoice());
+        scoreDiv.appendChild(span);
+        span.textContent = `Game number: ${gameNumber} Human score: ${humanScore} Computer score: ${computerScore}`
+        if (humanScore > computerScore) {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You win!"
+        } else if (computerScore > humanScore) {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You lose!"
+        } else {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You draw!"
+        }
+    } 
+    
+})
+
+
+scissors.addEventListener("click", () => {
+    if (gameNumber < 6) {
+        playRound("scissors", getComputerChoice());
+        scoreDiv.appendChild(span);
+        span.textContent = `Game number: ${gameNumber} Human score: ${humanScore} Computer score: ${computerScore}`
+    } else if (gameNumber = 5){
+        playRound("paper", getComputerChoice());
+        scoreDiv.appendChild(span);
+        span.textContent = `Game number: ${gameNumber} Human score: ${humanScore} Computer score: ${computerScore}`
+        if (humanScore > computerScore) {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You win!"
+        } else if (computerScore > humanScore) {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You lose!"
+        } else {
+            resultsDiv.appendChild(resultSpan);
+            resultSpan.textContent = "Game over! You draw!"
+        }
+    }
+})
